28f234eef8af3846d08c8dfe9bd1788e952ba12c
Student User <student@cti.pub.ro>
2017-03-31 12:21:31 +0300

    add skeleton

---
+++ app/src/main/java/practicaltest01var02/eim/system/cs/pub/ro/practicaltest01var02/PracticalTest01Var02MainActivity.java
@@ -0,0 +1,13 @@
+package practicaltest01var02.eim.system.cs.pub.ro.practicaltest01var02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracticalTest01Var02MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var02_main);
+    }
+}
